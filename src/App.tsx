4bbe7fa1e029ed8ea62d6f49f07480aebfd1d800
David Jones <david.jones@deptagency.com>
2024-05-30 15:57:27 -0400

    test events

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,20 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [events, setEvents] = useState([] as any);
+  const [events, setEvents] = useState([
+    {
+      id: 5,
+      title: "Release 2.1.5",
+      version: "2.1.5",
+      start: "2024-05-29T22:03:15",
+      end: "2024-05-29T23:00:00",
+      release_type: "regression",
+      team: "Spark Video Unit",
+      components: "V3 Foxipedia External Importer",
+      business_units: "FoxWeather",
+      build_owner: "",
+    },
+  ] as any);
 
   const handleDateClick = (stuff: any) => {
     setEvents([...events, stuff]);
@@ -16,7 +29,6 @@ function App() {
     <main>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin]}
-
         events={events}
         initialView="dayGridMonth"
         dateClick={handleDateClick}
